3504e032dee91406b8f7a698c197468587f26f79
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2022-03-30 02:50:51 +1000

    Edited styles

--- client/src/components/Toggle/Toggle.tsx
+++ client/src/components/Toggle/Toggle.tsx
@@ -1,26 +1,48 @@
-import { useMemo, useState } from 'react';
-import { IconContext } from 'react-icons';
 import { IoMenu, IoClose } from 'react-icons/io5';
-import './Toggle.scss';
+import styled, { css } from 'styled-components';
 
-function Toggle(): JSX.Element {
-  const [isActivated, setIsActivated] = useState(false);
-  const value = useMemo(
-    () => ({ className: isActivated ? 'icon-close' : 'icon-menu' }),
-    [isActivated]
-  );
+interface NavigationProps {
+  isOpen: boolean;
+  handleToggle(): void;
+}
+
+const StyledToggle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 36px;
+  right: 36px;
+  z-index: 3;
+  border-radius: 50px;
+  cursor: pointer;
+
+  :hover {
+    background-color: rgba(47, 47, 47, 1);
+    border-radius: 50px;
+    transition: all 0.2s ease-out;
+  }
+`;
+
+const IconStyles = css`
+  font-size: 3rem;
+  color: #ffcb74;
+  border-radius: 50px;
+`;
+
+const StyledIoMenu = styled(IoMenu)`
+  ${IconStyles}
+`;
+
+const StyledIoClose = styled(IoClose)`
+  ${IconStyles}
+`;
+
+function Toggle({ isOpen, handleToggle }: NavigationProps): JSX.Element {
   return (
-    <div
-      className="toggle"
-      role="menuitem"
-      tabIndex={0}
-      onClick={() => setIsActivated(!isActivated)}
-      onKeyPress={() => setIsActivated(!isActivated)}
-    >
-      <IconContext.Provider value={value}>
-        {isActivated ? <IoClose /> : <IoMenu />}
-      </IconContext.Provider>
-    </div>
+    <StyledToggle role="menuitem" tabIndex={0} onClick={handleToggle}>
+      {isOpen ? <StyledIoClose /> : <StyledIoMenu />}
+    </StyledToggle>
   );
 }
 
