65f117f2e360acc725227bc7ac086647995b0ab2
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2022-03-30 02:50:36 +1000

    Edited styles

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,37 +1,80 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.scss';
+import styled from 'styled-components';
+import navigations from './navigations.js';
+import Curtain from '../Curtain/Curtain';
+import Toggle from '../Toggle/Toggle';
 
-const navigations = [
-  {
-    id: 0,
-    title: 'About',
-    path: '/about',
-  },
-  {
-    id: 1,
-    title: 'Tools',
-    path: '/tools',
-  },
-  {
-    id: 2,
-    title: 'Contact',
-    path: '/contact',
-  },
-];
+interface NavigationProps {
+  isOpen: boolean;
+}
+
+const StyledNavbar = styled.div<NavigationProps>`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: ${({ isOpen }) => (isOpen ? 0 : '100vh')};
+  left: 0;
+  z-index: 3;
+  transition: ${({ isOpen }) =>
+    isOpen
+      ? 'bottom 0ms cubic-bezier(0.6, -0.28, 0.735, 0.045)'
+      : 'bottom 444ms cubic-bezier(0.6, -0.28, 0.735, 0.045)'};
+`;
+
+const StyledMenu = styled.nav<NavigationProps>`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  text-align: center;
+  pointer-events: ${({ isOpen }) => (isOpen ? 'all' : 'none')};
+  opacity: ${({ isOpen }) => (isOpen ? 1 : 0)};
+  transition: all 111ms ease-out 222ms;
+`;
+
+const StyledLink = styled(Link)`
+  display: block;
+  padding-left: 0.3em;
+  margin: 10px 0;
+  color: rgba(255, 203, 116, 1);
+  font-size: 2rem;
+  font-weight: 400;
+  letter-spacing: 0.3em;
+  text-decoration: none;
+  text-transform: uppercase;
+  transition: all 444ms ease-out;
+
+  :hover {
+    color: rgba(255, 203, 116, 1);
+    border-radius: 50px;
+    padding-left: 0.6em;
+    letter-spacing: 0.6em;
+  }
+`;
 
 function Navbar(): JSX.Element {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = (): void => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="Navbar">
-      <nav className="navigation-menu">
+    <StyledNavbar isOpen={isOpen}>
+      <Curtain isOpen={isOpen} />
+      <StyledMenu isOpen={isOpen}>
         {navigations.map((item) => {
           return (
-            <Link key={item.id} to={item.path} className="navbar-item">
+            <StyledLink key={item.id} to={item.path}>
               {item.title}
-            </Link>
+            </StyledLink>
           );
         })}
-      </nav>
-    </div>
+      </StyledMenu>
+      <Toggle isOpen={isOpen} handleToggle={handleToggle} />
+    </StyledNavbar>
   );
 }
 
---
+++ client/src/components/Navbar/navigations.js
@@ -0,0 +1,19 @@
+const navigations = [
+  {
+    id: 0,
+    title: 'About',
+    path: '/about',
+  },
+  {
+    id: 1,
+    title: 'Tools',
+    path: '/tools',
+  },
+  {
+    id: 2,
+    title: 'Work',
+    path: '/work',
+  },
+];
+
+export default navigations;
