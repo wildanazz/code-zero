057a06f480d277cbf3094c3dd7ef0bd53e4eebe7
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2022-04-01 03:32:25 +1000

    Edited particle style

--- client/src/components/Background/Background.tsx
+++ client/src/components/Background/Background.tsx
@@ -1,80 +1,8 @@
 import Particles from 'react-tsparticles';
+import config from './config.json';
 
 function Background(): JSX.Element {
-  return (
-    <Particles
-      options={{
-        background: {
-          color: {
-            value: '#111111',
-          },
-        },
-        fpsLimit: 120,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: false,
-              mode: 'push',
-            },
-            onHover: {
-              enable: false,
-              mode: 'repulse',
-            },
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: '#ffcb74',
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: 'none',
-            enable: true,
-            outMode: 'bounce',
-            random: false,
-            speed: 0.75,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: 'circle',
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: true,
-      }}
-    />
-  );
+  return <Particles options={config} />;
 }
 
 export default Background;
